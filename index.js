6d7cc4c0453aad17edb368354e05613f1ccef070
Jamin Chen <jamin.chen587@gmail.com>
2017-01-03 11:20:34 -0800

    testing

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.post('/webhook', function (req, res) {
                       }
                     ]
             });
+            console.log(event.postback);
         }
     }
     res.sendStatus(200);
