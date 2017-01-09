72486a7208f272f93633c1832abaea25c6e1693c
Jamin Chen <jamin.chen587@gmail.com>
2017-01-09 09:51:32 -0800

    testing

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook', function (req, res) {
             }
         } else if (event.postback) {
             payload = JSON.stringify(event.postback.payload);
-            console.log("payload");
+            console.log(payload);
             switch (payload) {
                 case "Send Message":
                     console.log("payload");
