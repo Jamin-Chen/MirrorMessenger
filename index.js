1534e4176eb565c81b342b4c52efa6250d7beae7
Jamin Chen <jamin.chen587@gmail.com>
2017-01-09 10:26:18 -0800

    testing

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ app.post('/webhook', function (req, res) {
         } else if (event.postback) {
             payload = JSON.stringify(event.postback.payload);
             payload = payload.trim();
+            payload = payload.slice(1, -1);
             console.log(typeof(payload));
             console.log(payload);
             if (payload == "SEND_MESSAGE") {
